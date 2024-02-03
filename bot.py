095bdf57309400df7365133dcc081c47f4fde2b3
kodsurfer <kodsurfer@ya.ru>
2024-02-03 03:24:28 +0300

    add url button

--- bot.py
+++ bot.py
@@ -1,3 +1,11 @@
 import telebot
+from telebot import types
 
 bot = telebot.TeleBot('BOT-TOKEN')
+
+@bot.message_handler(commands=['start'])
+def url(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton(text='crt works website', url='https://crt.works/')
+    markup.add(btn1)
+    bot.send_message(message.from_user.id, "Click button to visit our website!", reply_markup=markup)
