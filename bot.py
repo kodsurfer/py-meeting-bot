106259c527b8beb9459edef879863f1258adf888
kodsurfer <kodsurfer@ya.ru>
2024-02-05 01:28:19 +0300

    fix url link

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot('BOT-TOKEN')
 @bot.message_handler(commands=['start'])
 def url(message):
     markup = types.InlineKeyboardMarkup()
-    btn1 = types.InlineKeyboardButton(text='crt works website', url='https://crt.works/')
+    btn1 = types.InlineKeyboardButton(text='website', url='https://ya.ru/')
     markup.add(btn1)
     bot.send_message(message.from_user.id, "Click button to visit our website!", reply_markup=markup)
 
